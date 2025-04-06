7c45b24a2ca0ff6013697fd8518a8a98db51a6ee
swapnilgupta14 <swapnilguptaswag@gmail.com>
2025-04-06 11:47:08 +0530

    Refactor project structure: remove unused files and configurations, update app.json for Expo integration, and add new layout and index files for routing. Introduce global CSS styles and essential assets for the application.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    ['nativewind/babel', { mode: 'compileOnly' }],
-    'react-native-reanimated/plugin',
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
 };
